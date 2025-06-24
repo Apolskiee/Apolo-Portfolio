05adfc6dce4194094119aff1add2ff85231d4549
Apolo Trasmonte <apolgaming18@gmail.com>
2025-06-24 11:44:52 +0800

    Updated resume file

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String args[]) {
+        System.out.ptrintln(100 + 100 + "KeepOnCoding");
+        System.out.println("KeepOnCoding" + 100 + 100);
+
+    }
+}
